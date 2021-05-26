90143ab06eb1d649f99ed99295b06a6548b47e85
circletakesthesquare <13kochd@gmail.com>
2021-05-26 13:11:49 -0400

    Added truffle, initialized deploy file

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'rail avoid enrich enroll school debate wait average robot suffer endorse farm',
+  'https://rinkeby.infura.io/v3/7bfd0f4c602244d2a2a070f090b27dcf'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('attempting to deploy from account ', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('deployed to ', result.options.address)
+};
+deploy();
